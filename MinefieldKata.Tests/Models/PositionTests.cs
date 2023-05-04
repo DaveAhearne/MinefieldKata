4531f36a6a3f66073cb6692aff177871aca067f6
David Ahearne <david.ahearnex@gmail.com>
2023-05-04 17:36:23 +0100

    Added basic position & deleted default test file

---
+++ MinefieldKata.Tests/Models/PositionTests.cs
@@ -0,0 +1,23 @@
+﻿using MinefieldKata.Models;
+
+namespace MinefieldKata.Tests.Models
+{
+    public class PositionTests
+    {
+        [Fact]
+        public void WhenInitializingAPosition_TheDefaultCoordinatesForXare0AndYare0()
+        {
+            var position = new Position();
+            Assert.Equal(0, position.X);
+            Assert.Equal(0, position.Y);
+        }
+
+        [Fact]
+        public void WhenInitializingAPositionWithValues_TheCoordinatesAreSetToThoseValues()
+        {
+            var position = new Position(3, 5);
+            Assert.Equal(3, position.X);
+            Assert.Equal(5, position.Y);
+        }
+    }
+}
---
+++ MinefieldKata/Models/Position.cs
@@ -0,0 +1,20 @@
+﻿namespace MinefieldKata.Models
+{
+    public class Position
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Position()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
