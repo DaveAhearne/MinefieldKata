00bc58247b1245a9610fc001bc7e45d0d6b12c55
David Ahearne <david.ahearnex@gmail.com>
2023-05-04 17:33:23 +0100

    Initial Project Setup

---
+++ MinefieldKata/MinefieldGame.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace MinefieldKata
+{
+    public class MinefieldGame : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Running");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Stopping");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ MinefieldKata/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MinefieldKata
+{
+    class Program
+    {
+        private static readonly IConfiguration _configuration = new ConfigurationBuilder()
+            .AddJsonFile("config.json", optional: true, reloadOnChange: true)
+            .Build();
+
+        public static async Task Main()
+        {
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton(_configuration);
+
+                    services.AddHostedService<MinefieldGame>();
+                });
+
+            await builder.RunConsoleAsync();
+        }
+    }
+}
