eec57879f9b3a266c8e3e8fe28d032097d1e2b6d
David Ahearne <david.ahearnex@gmail.com>
2023-05-04 17:42:15 +0100

    Added player & damage player function

---
+++ MinefieldKata.Tests/Models/PlayerTests.cs
@@ -0,0 +1,27 @@
+﻿using MinefieldKata.Models;
+
+namespace MinefieldKata.Tests.Models
+{
+    public class PlayerTests
+    {
+        private Player player;
+
+        public PlayerTests()
+        {
+            player = new Player();
+        }
+
+        [Fact]
+        public void WhenCreatingANewPlayer_TheyIntiallyHave3Lives()
+        {
+            Assert.Equal(3, player.Lives);
+        }
+
+        [Fact]
+        public void WhenThePlayerIsDamaged_TheyLoseALife()
+        {
+            player.Damage();
+            Assert.Equal(2, player.Lives);
+        }
+    }
+}
---
+++ MinefieldKata/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace MinefieldKata.Models
+{
+    public interface IPlayer
+    {
+        int Lives { get; }
+        void Damage();
+    }
+
+    public class Player : IPlayer
+    {
+        public int Lives { get; private set; } = 3;
+
+        public void Damage()
+        {
+            Lives--;
+        }
+    }
+}
