25a6c429cba9e3e7990809a2e9f795f794981d51
David Ahearne <david.ahearnex@gmail.com>
2023-05-04 18:44:29 +0100

    Added basic game states with loss check for player being out of lives

---
+++ MinefieldKata.Tests/Models/GameStateHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿using MinefieldKata.Enums;
+using MinefieldKata.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinefieldKata.Tests.Models
+{
+	public class GameStateHandlerTests
+	{
+        private Mock<IPlayer> mockPlayer;
+
+        private GameStateHandler gameStateHandler;
+
+        public GameStateHandlerTests()
+        {
+            mockPlayer = new Mock<IPlayer>();
+
+            gameStateHandler = new GameStateHandler(mockPlayer.Object);
+        }
+
+        [Fact]
+        public void WhenThePlayerHasMoreThanZeroLives_TheGameStateIsLeftAsPlaying()
+        {
+            mockPlayer.SetupGet(x => x.Lives).Returns(2);
+            gameStateHandler.UpdateGameState();
+
+            Assert.Equal(GameState.Playing, gameStateHandler.State);
+        }
+
+
+        [Fact]
+        public void WhenThePlayerHasZeroLives_TheGameStateIsSetToLost()
+        {
+            mockPlayer.SetupGet(x => x.Lives).Returns(0);
+            gameStateHandler.UpdateGameState();
+
+            Assert.Equal(GameState.Lost, gameStateHandler.State);
+        }
+    }
+}
---
+++ MinefieldKata/Models/GameStateHandler.cs
@@ -0,0 +1,30 @@
+﻿using MinefieldKata.Enums;
+
+namespace MinefieldKata.Models
+{
+    public interface IGameStateHandler
+    {
+        GameState State { get; }
+        void UpdateGameState();
+    }
+
+    public class GameStateHandler : IGameStateHandler
+    {
+        private readonly IPlayer _player;
+        public GameState State { get; private set; } = GameState.Playing;
+
+        public GameStateHandler(IPlayer player)
+        {
+            _player = player;
+        }
+
+        public void UpdateGameState()
+        {
+            if (_player.Lives <= 0)
+            {
+                State = GameState.Lost;
+                return;
+            }
+        }
+    }
+}
